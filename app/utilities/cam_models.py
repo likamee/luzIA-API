35a2dbf89be91a45612550406d041c57c27cd13b
Gabriel Aranha <talk@gabrielaranha.com>
2023-12-22 12:31:03 -0300

    changing the GAP layer from function to class

--- app/utilities/cam_models.py
+++ app/utilities/cam_models.py
@@ -2,6 +2,21 @@
 from tensorflow.keras import backend as K
 from tensorflow.keras.metrics import AUC, BinaryAccuracy
 from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import Layer
+
+class GlobalAveragePooling(Layer):
+    def __init__(self, **kwargs):
+        super(GlobalAveragePooling, self).__init__(**kwargs)
+
+    def call(self, inputs):
+        return K.mean(inputs, axis=(2, 3))
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0:2]
+
+    def get_config(self):
+        base_config = super(GlobalAveragePooling, self).get_config()
+        return base_config
 
 metrics = [
       BinaryAccuracy(name='accuracy'),
@@ -10,14 +25,6 @@ metrics = [
 
 def load_saved_model(model_path):
     """Load the saved model from the specified path."""
-    custom_objects = {'global_average_pooling': global_average_pooling}
-    return load_model(model_path, custom_objects=custom_objects, compile=True, safe_mode=False)
+    custom_objects = {'Custom>GlobalAveragePooling': GlobalAveragePooling}
+    return load_model(model_path, custom_objects=custom_objects)
 
-
-
-def global_average_pooling(x):
-    return K.mean(x, axis=(2, 3))
-
-
-def global_average_pooling_shape(input_shape):
-    return input_shape[0:2]
